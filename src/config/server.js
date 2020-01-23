3a751631e3db79ce4a5268efdc170c1638b92c69
Thiago Almeida <thiago.almeida@coopersystem.com.br>
2020-01-23 17:19:33 -0300

    enabled cors

--- src/config/server.js
+++ src/config/server.js
@@ -3,6 +3,8 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const  server  = express()
+const allowCors = require('./cors')
+server.use(allowCors)
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
